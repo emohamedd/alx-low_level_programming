cf60663eb4c3e18d0fdcb6b19adace4b07e2bf01
emohamedd <a.elgaouej@gmail.com>
2023-03-16 08:50:32 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
-
-int main()
+#include <stdio>
+/**
+ * main - Entry point
+ * Description: 'prints a string the standard output'
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
