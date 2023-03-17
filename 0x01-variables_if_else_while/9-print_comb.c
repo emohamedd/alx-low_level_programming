6ffeadbbca2cb785a2e90e14626d7b344c4ef36f
emohamedd <a.elgaouej@gmail.com>
2023-03-17 10:47:55 -0700

    print number ,

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,17 @@
  */
 int main(void)
 {
-	int i = '0';
+	int i = 0;
 
-	while (i <= '9')
+	while (i <= 9)
 	{
-		putchar(i);
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		i++;
-		putchar(',');
-		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
