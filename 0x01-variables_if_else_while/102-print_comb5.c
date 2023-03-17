23d39046e35150e143ce963a052a758d28b1c21a
emohamedd <a.elgaouej@gmail.com>
2023-03-17 12:54:17 -0700

    last one

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Description: Task
+  *Return: always 0
+  */
+int main()
+{
+	int	a;
+	int	b;
+
+	a = -1;
+	while (a++ < 98)
+	{
+		b = a;
+		while (b++ < 99)
+		{
+			putchar ('0' + (a / 10));
+			putchar ('0' + (a % 10));
+			putchar (' ');
+			putchar ('0' + (b / 10));
+			putchar ('0' + (b % 10));
+			if (a != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
