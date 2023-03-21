5bfe0592355329b13eab35fe62f07db4f28d17b1
emohamedd <a.elgaouej@gmail.com>
2023-03-21 06:58:43 -0700

    Finish TAsk

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,21 @@
 
 int print_sign(int n)
 {
+	int r;
 	if (n > 0)
 	{
 		printf("+");
-		return (1);
+		r = 1;
 	}
 	else if (n == 0)
 	{
-		printf("0\n");
-		return (0);
+		printf("0");
+		r = 0;
 	}
 	else
 	{
-		printf("-\n");
-		return (-1);
+		printf("-");
+		r = -1;
 	}
+	return (r);
 }
