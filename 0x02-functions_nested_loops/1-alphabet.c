b3c114cbda963da7456b11ff7ff0399b122a4f3b
emohamedd <a.elgaouej@gmail.com>
2023-03-21 04:31:28 -0700

    Finish TAsk

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *Description: Task
+  *Return: always 0
+*/
+
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		printf("%c", a);
+		a++;
+	}
+	putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+void print_alphabet(void);
+#endif
