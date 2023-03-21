3ef1bfaa37bc7063372aee10bc52eb911c7788a9
emohamedd <a.elgaouej@gmail.com>
2023-03-21 06:00:18 -0700

    Finish TAsk

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - prints the alphabet in lowercase
+ *
+ * Return: void
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
