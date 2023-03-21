eb71b423b8198d646b9b53673ceaad4d573aa712
emohamedd <a.elgaouej@gmail.com>
2023-03-21 04:36:04 -0700

    Finish TAsk

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,6 +9,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+void print_alphabet(void);
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
@@ -18,5 +19,4 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
-void print_alphabet(void);
 #endif
