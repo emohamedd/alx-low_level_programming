bac7f54b66e25a792945feb71d9812db4196f8c3
emohamedd <a.elgaouej@gmail.com>
2023-03-21 11:52:30 -0700

    Finish TAsk

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints the Time
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i = 0;
+	int j = 0;
+	int m = 0;
+
+	while (i < 10)
+	{
+		j = 0;
+		while(j < 10)
+		{
+			m = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (m <= 9)
+					_putchar(' ');
+			}
+			if (m >= 0 && m < 10)
+				_putchar(m + '0');
+			else
+			{
+				_putchar(m / 10 + '0');
+				_putchar(m % 10 + '0');
+			}
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
+int main()
+{
+	times_table();
+}
