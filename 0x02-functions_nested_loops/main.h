e0bd37b79e9ec14fcd2e865dacf42b2c3d40753d
emohamedd <a.elgaouej@gmail.com>
2023-03-21 04:40:40 -0700

    Finish TAsk

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <unistd.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-void print_alphabet(void);
-int main(void)
-{
-        print_alphabet();
-        return (0);
-}
-
+#include <stdio.h>
+void	_putchar(int c);
+void	print_alphabet(void);
 #endif
