a1dccee804156b2dd14ca4a08f25e6ad46e0a581
emohamedd <a.elgaouej@gmail.com>
2023-03-21 12:51:14 -0700

    Finish TAsk

---
+++ 0x02-functions_nested_loops/11-printf_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_to_98 - adds two integers
+*@n: first number
+*Return: returns the sum of a and b
+*/
+
+
+
+void print_to_98(int n)
+{
+
+	while (n != 99)
+	{
+		printf("%d, ", n);
+		if (n > 98)
+			n--;
+		else
+			n++;
+	}
+	printf("%d\n", n);
+}
