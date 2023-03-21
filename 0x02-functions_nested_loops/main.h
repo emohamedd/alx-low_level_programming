2b2e1351e34ccb6e7c713fe99593d22eb0a921d5
emohamedd <a.elgaouej@gmail.com>
2023-03-21 10:40:12 -0700

    Finish TAsk

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+ #ifndef MAIN_H
+ #define MAIN_H
 
-#include <stdio.h>
-
+ #include <stdio.h>
+#include <unistd.h>
+void _putchar(int c)
+{
+	write(1, &c, 1);
+}
 void	_putchar(int c);
 void	print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,4 +15,5 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int a);
 int print_last_digit(int);
+void jack_bauer(void);
 #endif
