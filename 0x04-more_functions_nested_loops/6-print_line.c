4346806fec92d0ff47a98fd88598a9754eb0fb85
emohamedd <a.elgaouej@gmail.com>
2023-03-23 08:59:59 -0700

    Finish TAsk

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_line - print numbers from 0 to 14 ten times
+ *@n: variable
+ *Return: nothing
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+		_putchar('\n');
+	while (i <= n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
