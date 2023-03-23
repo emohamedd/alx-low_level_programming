5dd66a96325fceabcd9bb5c47c38958527b702db
emohamedd <a.elgaouej@gmail.com>
2023-03-23 09:56:59 -0700

    Finish TAsk

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *main - the main program
+ *Return: nothing
+ */
+
+int main(void)
+{
+
+	int i = 0;
+
+	while (i < 100)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
