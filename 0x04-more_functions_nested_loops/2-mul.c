f736da4f7a09031eb87bbb191d27491cd5f68db1
emohamedd <a.elgaouej@gmail.com>
2023-03-23 07:53:29 -0700

    Finish TAsk

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - prints the alphabet in lowercase
+ * @a: the variable
+ * @b: variable
+ * Return: value
+ */
+
+int mul(int a, int b)
+{
+	int store;
+
+	store = a * b;
+	return (store);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
+
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
 
