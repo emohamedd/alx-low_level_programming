8542933d2475dd35fdbaf447465a8dfb0fd8a103
emohamedd <a.elgaouej@gmail.com>
2023-03-23 19:42:48 -0700

    Finish TAsk

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_number - print a number
+ *@n: the number to be printed
+ *Return: nothing
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+		num = n;
+	if (num > 9)
+	{
+		print_number(num / 10);
+		print_number(num % 10);
+	}
+	else
+		_putchar(num + '0');
+}
