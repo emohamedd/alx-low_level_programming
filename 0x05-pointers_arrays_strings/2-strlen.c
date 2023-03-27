7838d9023d77694a4602700949970efe835d1274
emohamedd <a.elgaouej@gmail.com>
2023-03-27 12:22:18 -0700

    Finish TAsk

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_strlen - lenght of a string
+ *@s: variable
+ *Return: i
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (i < s[i])
+	{
+		i++;
+	}
+	return (i);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+
 #endif
