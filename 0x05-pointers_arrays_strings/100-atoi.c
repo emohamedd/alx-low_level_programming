ddeb63aea590944239f5c1bd3d3a4e3cdfa44dd3
emohamedd <a.elgaouej@gmail.com>
2023-03-27 16:51:03 -0700

    Finish TAsk

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_atoi - converts a string to an int
+ *@s: the string to convert
+ *Return: returns an int
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	unsigned int res = 0;
+	int sign = 1;
+
+	while (s[i] && !(s[i] >= '0' && s[i] <= '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (s[i] && (s[i] >= '0' && s[i] <= '9'))
+	{
+		res = (res * 10) + (s[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
