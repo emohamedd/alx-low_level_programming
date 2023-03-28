b8af22521f864cde3404c1b3d10a52cbbe82ab41
emohamedd <a.elgaouej@gmail.com>
2023-03-28 11:43:50 -0700

    Finish TAsk

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program generate random password
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    char password[101];
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#!?$";
+    int i, sum = 0;
+
+    srand(time(NULL));
+
+    for (i = 0; i < 100; i++)
+    {
+        int index = rand() % (sizeof(charset) - 1);
+        password[i] = charset[index];
+        sum += charset[index];
+    }
+
+    password[100] = '\0';
+
+    if (sum < 2772)
+    {
+        password[99] = 2772 - sum;
+    }
+
+    printf("Generated Password: %s\n", password);
+
+    return 0;
+}
