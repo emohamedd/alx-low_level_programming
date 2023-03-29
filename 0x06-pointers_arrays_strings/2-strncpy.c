406d9abad77a2d8aca4ebae0649e710abb58e97b
emohamedd <a.elgaouej@gmail.com>
2023-03-29 11:34:46 -0700

    Finish TAsk

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -30,21 +30,3 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	return (dest);
 }
-int main(void)
-{
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
-
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strncat(s1, s2, 1);
-    printf("%s\n", s1);
-    printf("%s", s2);
-    printf("%s\n", ptr);
-    ptr = _strncat(s1, s2, 1024);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    return (0);
-}
