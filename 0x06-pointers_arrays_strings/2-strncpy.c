84a40ea6e4de43062a507e3533faeeb200033c00
emohamedd <a.elgaouej@gmail.com>
2023-03-29 11:34:11 -0700

    Finish TAsk

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _strncat - a function that print n elem of array
+ * _strncpy - a function that print n elem of array
  * @dest: pointer variable
  * @src: variable
  * @n: variable
  * Return: void return nothing
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 
 	int i = 0;
