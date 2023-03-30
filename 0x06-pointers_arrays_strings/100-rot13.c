552731d9c6f295b4c80c6b980eb1ff67bc71ab49
emohamedd <a.elgaouej@gmail.com>
2023-03-30 13:57:41 -0700

    Finish TAsk

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *rot13 - Main Function
+ *@str: string
+ *Return: Return
+ */
+
+
+char *rot13(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'M')
+			str[i] += 13;
+		else if (str[i] >= 'a' && str[i] <= 'm')
+			str[i] += 13;
+		else if (str[i] >= 'N' && str[i] <= 'Z')
+			str[i] -= 13;
+		else if (str[i] >= 'n' && str[i] <= 'z'
+			str[i] -= 13;
+		i++;
+	}
+	return (str);
+}
