19d490268bde99995bc06ed9fb23977471a26ec3
emohamedd <a.elgaouej@gmail.com>
2023-04-03 14:37:05 -0700

    Finish TAsk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,4 +5,5 @@
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
 #endif
