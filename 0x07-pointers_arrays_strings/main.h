849d80f605d1215c15b6ebf12fccaab686ce96d8
emohamedd <a.elgaouej@gmail.com>
2023-04-03 15:13:14 -0700

    Finish TAsk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stddef.h>
+
 void _putchar(int c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -10,4 +11,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 #endif
+
