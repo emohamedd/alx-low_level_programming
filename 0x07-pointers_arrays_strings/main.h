f2b7f1c5f23195cad9928f222bbe8fbe8434b233
emohamedd <a.elgaouej@gmail.com>
2023-04-03 15:00:04 -0700

    Finish TAsk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,4 +7,5 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 #endif
