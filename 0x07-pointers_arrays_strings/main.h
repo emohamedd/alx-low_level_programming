0a07f22b7aca6babf5a95bb01d33b8d116b4ff3e
emohamedd <a.elgaouej@gmail.com>
2023-04-03 15:03:14 -0700

    Finish TAsk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stddef.h>
+void _putchar(int c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
