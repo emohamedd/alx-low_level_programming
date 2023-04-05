436ffc50cc90a0d19829356d1de6b41a2c29aca1
emohamedd <a.elgaouej@gmail.com>
2023-04-05 10:19:56 -0700

    Finish TAsk

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ *_print_rev_recursion - prints
+ * @s: an array of pointers
+ *Return: nothing void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(++s);
+	_putchar(*s);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,4 +3,5 @@
 #include <unistd.h>
 void _puts_recursion(char *s);
 void _putchar(char c);
+void _print_rev_recursion(char *s);
 #endif
