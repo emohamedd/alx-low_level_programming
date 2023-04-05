dfe960784cadbe8e9f73a49eaf1af60ae392521b
emohamedd <a.elgaouej@gmail.com>
2023-04-05 10:55:27 -0700

    Finish TAsk

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ *_print_rev_recursion - prints
+ * @s: an array of pointers
+ *Return: nothing void
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return(_strlen_recursion(s + 1) + 1);
+
+}
