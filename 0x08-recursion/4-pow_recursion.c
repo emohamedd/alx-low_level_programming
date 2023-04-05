a166ded693e17b2e7fc6e5f7a227f16df311fc11
emohamedd <a.elgaouej@gmail.com>
2023-04-05 11:57:02 -0700

    Finish TAsk

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * _pow_recursion - prints
+ * @x: var
+ * @y: var
+*Return: nothing void
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
