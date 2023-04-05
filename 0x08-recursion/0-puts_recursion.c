7a3f474d45875f8adb3c5307435db2b8b54b70a6
emohamedd <a.elgaouej@gmail.com>
2023-04-04 23:48:25 -0700

    Finish TAsk

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-	while (s[i])
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(++s);
 }
