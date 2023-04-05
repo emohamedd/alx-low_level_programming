18624119d9d6882db84c1c96623adf5317590514
emohamedd <a.elgaouej@gmail.com>
2023-04-05 11:45:58 -0700

    Finish TAsk

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <unistd.h>
+
 void _puts_recursion(char *s);
 void _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
 #endif
