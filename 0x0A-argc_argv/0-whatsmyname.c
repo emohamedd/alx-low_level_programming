0ccef69f633e6386837c50c364109ed2c64ab01b
emohamedd <a.elgaouej@gmail.com>
2023-04-06 10:59:50 -0700

    Finish TAsk

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/* By EMOHAMEDD */
+
+/**
+ *  - The Function
+ * @:  The variable
+ * Return: The Return value/void
+ */
+
+int main(int c, char **v)
+{
+	if (c > 0)
+		printf("%s\n", *v);
+	return (0);
+}
