5c056744b618466d7e199232e468a50ab1667a33
emohamedd <a.elgaouej@gmail.com>
2023-04-11 15:47:39 -0700

    Finish TAsk

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,8 +1,29 @@
+#include "main.h"
+
 /* By EMOHAMEDD */
 
 /**
- * main - The Function
- * @c:  The variable
- * @v: The Variable
+ * create_array - The Function
+ * @size:  The variable
+ * @c: The Variable
  * Return: The Return value/void
  */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	i = 0;
+	if (size == 0)
+		return (NULL);
+	arr = malloc(size * sizeof(char));
+	if (!arr)
+		return (NULL);
+	while (i < size)
+	{
+		arr[i] = c;
+		i++;
+	}
+	return (arr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-char *create_array(unsigned int size, char c);	
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int	**alloc_grid(int width, int height);
+void gree_grid(int **grid, int height);
+char *argstostr(int ac, char **avv);
+char **strtow(char *str);
+
 #endif
