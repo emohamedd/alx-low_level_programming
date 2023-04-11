134a5dda75df9d457a17021e0f417788ea9773b0
emohamedd <a.elgaouej@gmail.com>
2023-04-11 15:59:41 -0700

    Finish TAsk

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,8 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 /* By EMOHAMEDD */
 
 /**
- * main - The Function
- * @c:  The variable
- * @v: The Variable
+ * strdup - The Function
+ * @str:  The variable
  * Return: The Return value/void
  */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char *copy;
+
+	if (!str)
+		return (NULL);
+	while (str[j])
+		j++;
+	copy = malloc((j + 1) * sizeof(char));
+	if (!copy)
+		return (NULL);
+	while (str[i])
+	{
+		copy[i] = str[i];
+		i++;
+	}
+	copy[i] = str[i];
+	return (copy);
+}
