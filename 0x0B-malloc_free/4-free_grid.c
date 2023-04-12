fbdacd0c25615b9340e0b6ae24b176dd5e38df84
emohamedd <a.elgaouej@gmail.com>
2023-04-11 17:55:24 -0700

    Finish TAsk

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,8 +1,23 @@
+#include "main.h"
+
 /* By EMOHAMEDD */
 
 /**
- * main - The Function
- * @c:  The variable
- * @v: The Variable
+ * free_grid - The Function
+ * @grid:  The variable
+ * @height: The Variable
  * Return: The Return value/void
  */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+}
+
