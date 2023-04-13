19d8f002c842563848bc5cd966c3684288364d9e
emohamedd <a.elgaouej@gmail.com>
2023-04-13 06:47:06 -0700

    Finish TAsk

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - The Function
+ * @b:  The variable
+ * Return: The Return value/void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *memory;
+
+	memory = malloc(b);
+	if (!memory)
+		exit(98);
+	return (memory);
+
+}
