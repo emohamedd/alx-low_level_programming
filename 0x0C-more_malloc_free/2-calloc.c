e95215af6dfc4ccacf22c7c21e739b87f22a5f3e
emohamedd <a.elgaouej@gmail.com>
2023-04-13 08:01:14 -0700

    Finish TAsk

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,8 +5,9 @@
 /* By EMOHAMEDD */
 
 /**
- * malloc_checked - The Function
- * @b:  The variable
+ * _calloc - The Function
+ * @nmemb:  The variable
+ * @size:  The variable
  * Return: The Return value/void
  */
 
@@ -26,42 +27,4 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		i++;
 	}
 	return (p);
-}
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *a;
-
-    a = _calloc(98, sizeof(char));
-    strcpy(a, "Best");
-    strcpy(a + 4, " School! :)\n");
-    a[97] = '!';
-    simple_print_buffer(a, 98);
-    free(a);
-    return (0);
 }
