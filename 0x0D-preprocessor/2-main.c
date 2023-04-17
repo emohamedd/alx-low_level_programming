a8777733852a89c03ea07b6519067c5eca165caf
emohamedd <a.elgaouej@gmail.com>
2023-04-17 10:12:04 -0700

    Finish TAsk

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* EMOHAMEDD */
+
+/**
+ *main - main point
+ *Return: success
+ */
+
+int main(void)
+{
+
+	char *nfile = __FILE__;
+
+	printf("%s\n", nfile);
+	return (0);
+}
