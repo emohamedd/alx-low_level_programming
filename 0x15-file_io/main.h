fbf7bc96750c482bdf70988e710ede4c16737090
emohamedd <a.elgaouej@gmail.com>
2023-05-08 09:48:25 -0700

    in the first try

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+void _putchar(int c);
+int create_file(const char *filename, char *text_content);
+#endif
